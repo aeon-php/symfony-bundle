cbf6d2cada30baf0a4fbfdb88aeb0efb8631fb9e
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-11-06 23:03:04 +0100

    Fixed failing tests at older version of symfony

--- tests/Aeon/Symfony/AeonBundle/Tests/Functional/App/TestAppKernel.php
+++ tests/Aeon/Symfony/AeonBundle/Tests/Functional/App/TestAppKernel.php
@@ -13,7 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
 final class TestAppKernel extends BaseKernel
 {
@@ -61,7 +61,7 @@ final class TestAppKernel extends BaseKernel
         ]);
     }
 
-    protected function configureRoutes(RoutingConfigurator $routes) : void
+    protected function configureRoutes(RouteCollectionBuilder $routes) : void
     {
         $routes->add('not_holiday', '/not-holiday')->controller([$this, 'notHoliday']);
     }
